4200266b933c4ba067252292c5f9368348240651
downerpower <marie.beilina3030@gmail.com>
2022-11-09 10:43:33 +0300

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '98f61783-1143-460e-b062-4d46ac6536c0';
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/views/resultsView.js
@@ -0,0 +1,17 @@
+import icons from 'url:../../img/icons.svg';
+
+import View from './View';
+import previewView from './previewView';
+
+class ResultsView extends View {
+   _parentElement = document.querySelector('.results');
+   _errorMessage = 'No recipies found for your query! Please try again.';
+   _message = ' '
+
+   _generateMarkup() {
+      console.log(this._data)
+      return this._data.map(result => previewView.render(result, false)).join('')
+   }
+}
+
+export default new ResultsView();
